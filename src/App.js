db67281d5683475d61241593dba33d92f0018ec5
Quang <nmquang2012@gmail.com>
2021-10-21 16:34:10 +0700

    Design Trello Front-end.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,108 @@
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <h2>Hello</h2>
+    <div className="trello-app">
+      <nav className="navbar app">App Bar</nav>
+      <nav className="navbar board">Board Bar</nav>
+      <div className="board-columns">
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <span className="task-img"></span>
+              <p> Title: Hi guys! </p>
+            </li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <span className="task-img"></span>
+              <p> Title: Hi guys! </p>
+            </li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <span className="task-img"></span>
+              <p> Title: Hi guys! </p>
+            </li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <span className="task-img"></span>
+              <p> Title: Hi guys! </p>
+            </li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <span className="task-img"></span>
+              <p> Title: Hi guys! </p>
+            </li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <span className="task-img"></span>
+              <p> Title: Hi guys! </p>
+            </li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <span className="task-img"></span>
+              <p> Title: Hi guys! </p>
+            </li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+      </div>
     </div>
   );
 }
